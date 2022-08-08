22a4a2752dcb55717d6cfa8619b95bed3ada2649
aman <amanshah9712@gmail.com>
2022-08-08 17:48:52 +0530

    change css

--- system/classes/framework.php
+++ system/classes/framework.php
@@ -1,126 +1,139 @@
+
 <?php
-error_reporting(0);
+
 class framework {
 
    public function view($viewName, $data = []){
 
-     if(file_exists("../application/views/" . $viewName . ".php")){
-        
-        require_once "../application/views/$viewName.php";
+    if(file_exists("../application/views/" . $viewName . ".php")){
 
-     } else {
-        echo "<div style='margin:0;padding: 10px;background-color:silver;'>Sorry $viewName.php file not found </div>";
-     }
+       require_once "../application/views/$viewName.php";
 
-   }
+    } else {
+       echo "<div style='margin:0;padding: 10px;background-color:silver;'>Sorry $viewName.php file not found </div>";
+    }
 
-   public function model($modelName){
+ }
 
-      if(file_exists("../application/models/" . $modelName . ".php")){
+ public function model($modelName){
 
-        require_once "../application/models/$modelName.php";
-        return new $modelName;
+   if(file_exists("../application/models/" . $modelName . ".php")){
 
-      } else {
-        echo "<div style='margin:0;padding: 10px;background-color:silver;'>Sorry $modelName.php file not found </div>";
-      }
+    require_once "../application/models/$modelName.php";
+    return new $modelName;
 
-   }
+ } else {
+    echo "<div style='margin:0;padding: 10px;background-color:silver;'>Sorry $modelName.php file not found </div>";
+ }
 
-   public function input($inputName){
+}
 
-      if($_SERVER['REQUEST_METHOD'] == "POST" || $_SERVER['REQUEST_METHOD'] == 'post'){
+public function input($inputName){
 
-         return trim(strip_tags($_POST[$inputName]));
+   if($_SERVER['REQUEST_METHOD'] == "POST" || $_SERVER['REQUEST_METHOD'] == 'post'){
 
-      } else if($_SERVER['REQUEST_METHOD'] == 'GET' || $_SERVER['REQUEST_METHOD'] == 'get'){
+      return trim(strip_tags($_POST[$inputName]));
 
-         return trim(strip_tags($_GET[$inputName]));
+   } else if($_SERVER['REQUEST_METHOD'] == 'GET' || $_SERVER['REQUEST_METHOD'] == 'get'){
 
-      }
+      return trim(strip_tags($_GET[$inputName]));
 
    }
 
-   public function helper($helperName){
+}
+public function inputCk($inputName){
 
-      if(file_exists("../system/helpers/".$helperName.".php")){
+   if($_SERVER['REQUEST_METHOD'] == "POST" || $_SERVER['REQUEST_METHOD'] == 'post'){
 
-         require_once "../system/helpers/".$helperName.".php";
+      return trim($_POST[$inputName]);
 
-      } else {
-         echo "<div style='margin:0;padding: 10px;background-color:silver;'>Sorry helper $helperName file not found </div>";
-      }
+   } else if($_SERVER['REQUEST_METHOD'] == 'GET' || $_SERVER['REQUEST_METHOD'] == 'get'){
+
+      return trim($_GET[$inputName]);
 
    }
+
+}
+
+public function helper($helperName){
+
+   if(file_exists("../system/helpers/".$helperName.".php")){
+
+      require_once "../system/helpers/".$helperName.".php";
+
+   } else {
+      echo "<div style='margin:0;padding: 10px;background-color:silver;'>Sorry helper $helperName file not found </div>";
+   }
+
+}
 
    // Set session
-   public function setSession($sessionName, $sessionValue){
+public function setSession($sessionName, $sessionValue){
 
 
-       if(!empty($sessionName) && !empty($sessionValue)){
-          $_SESSION[$sessionName] = $sessionValue;
-       }
+  if(!empty($sessionName) && !empty($sessionValue)){
+     $_SESSION[$sessionName] = $sessionValue;
+  }
 
-   }
+}
 
    // Get session
-   public function getSession($sessionName){
+public function getSession($sessionName){
+if(!empty($sessionName)){
+return $_SESSION[$sessionName];
+ }
 
-     if(!empty($sessionName)){
-        return $_SESSION[$sessionName];
-     }
-
-   }
+}
 
    // Unset session
-   public function unsetSession($sessionName){
+public function unsetSession($sessionName){
 
-      if(!empty($sessionName)){
-         
-         unset($_SESSION[$sessionName]);
+   if(!empty($sessionName)){
 
-      }
+      unset($_SESSION[$sessionName]);
 
    }
+
+}
 
    // Destroy whole sessions
-   public function destroy(){
+public function destroy(){
 
-      session_destroy();
+   session_destroy();
 
-   }
+}
 
 
    // Set flash message
-   public function setFlash($sessionName, $msg){
+public function setFlash($sessionName, $msg){
 
-      if(!empty($sessionName) && !empty($msg)){
+   if(!empty($sessionName) && !empty($msg)){
 
-         $_SESSION[$sessionName] = $msg;
-
-      }
+      $_SESSION[$sessionName] = $msg;
 
    }
+
+}
 
    //Show flash message
-   public function flash($sessionName, $className){
+public function flash($sessionName, $className){
 
-      if(!empty($sessionName) && !empty($className) && isset($_SESSION[$sessionName])){
-         
-         $msg = $_SESSION[$sessionName];
-         
-         echo "<div class='". $className ."'>".$msg."</div>";
-         unset($_SESSION[$sessionName]);
+   if(!empty($sessionName) && !empty($className) && isset($_SESSION[$sessionName])){
 
-      }
+      $msg = $_SESSION[$sessionName];
+
+      echo "<div class='". $className ."'>".$msg."</div>";
+      unset($_SESSION[$sessionName]);
 
    }
 
-   public function redirect($path){
+}
+
+public function redirect($path){
 
    header("location:" . BASEURL . "/".$path);
 
-   }
+}
 
 
 }
