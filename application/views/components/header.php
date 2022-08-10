6d8f47ef253c4975bda534f65e4edae9a8bcc24f
aman <amanshah9712@gmail.com>
2022-08-10 15:31:28 +0530

    so many changes

--- application/views/components/header.php
+++ application/views/components/header.php
@@ -1,4 +1,4 @@
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <div class="container-fluid">
     <div class="row bg-secondary py-2 px-xl-5">
         <div class="col-lg-6 d-none d-lg-block">
@@ -234,8 +234,9 @@
 
       </div>
       <div class="modal-footer d-flex justify-content-center">
-        <button class="btn btn-default">Login</button>
+        <button class="btn btn-default" id="login">Login</button>
     </div>
+
 </form>
 </div>
 </div>
